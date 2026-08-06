c40325d83b430c45e415bc6a97e30ec0668a2f70
HassanOigag <hassanoigag0@gmail.com>
2026-08-06 16:19:08 +0100

    align columns in long format

--- main.c
+++ main.c
@@ -5,11 +5,66 @@
 
 
 
+char *owner_name(uid_t uid)
+{
+   struct passwd *pw = getpwuid(uid);
+   if (pw)
+      return pw->pw_name;
+   return "?";
+}
+
+char *group_name(gid_t gid)
+{
+   struct group *gr = getgrgid(gid);
+   if (gr)
+      return gr->gr_name;
+   return "?";
+}
+
+int num_width(long n)
+{
+   int w = 1;
+   while (n >= 10)
+   {
+      n /= 10;
+      w++;
+   }
+   return w;
+}
+
+void print_pad(int n)
+{
+   while (n-- > 0)
+      ft_printf(" ");
+}
+
+void get_widths(t_list *files, int *wl, int *wo, int *wg, int *ws)
+{
+   *wl = 0;
+   *wo = 0;
+   *wg = 0;
+   *ws = 0;
+   while (files)
+   {
+      t_file *f = files->content;
+      int l = num_width(f->nlink);
+      int o = ft_strlen(owner_name(f->uid));
+      int g = ft_strlen(group_name(f->gid));
+      int s = num_width(f->size);
+      if (l > *wl) *wl = l;
+      if (o > *wo) *wo = o;
+      if (g > *wg) *wg = g;
+      if (s > *ws) *ws = s;
+      files = files->next;
+   }
+}
+
 void printfiles(t_list *targets, int extended_print)
 {
    t_list *tmp = targets;
+   int wl, wo, wg, ws;
 
-
+   get_widths(targets, &wl, &wo, &wg, &ws);
    while (tmp)
    {
       t_file *file = (t_file *) tmp->content;
@@ -17,26 +72,32 @@ void printfiles(t_list *targets, int extended_print)
       if (extended_print)
       {
          ft_printf("%c", S_ISDIR(file->mode) ? 'd' : '-');
-   
+
          ft_printf("%c", (file->mode & S_IRUSR) ? 'r' : '-');
          ft_printf("%c", (file->mode & S_IWUSR) ? 'w' : '-');
          ft_printf("%c", (file->mode & S_IXUSR) ? 'x' : '-');
-   
+
          ft_printf("%c", (file->mode & S_IRGRP) ? 'r' : '-');
          ft_printf("%c", (file->mode & S_IWGRP) ? 'w' : '-');
          ft_printf("%c", (file->mode & S_IXGRP) ? 'x' : '-');
-   
+
          ft_printf("%c", (file->mode & S_IROTH) ? 'r' : '-');
          ft_printf("%c", (file->mode & S_IWOTH) ? 'w' : '-');
          ft_printf("%c", (file->mode & S_IXOTH) ? 'x' : '-');
-   
+
          ft_printf(" ");
+         print_pad(wl - num_width(file->nlink));
          ft_printf("%d", file->nlink);
          ft_printf(" ");
-         ft_printf("%s", getpwuid(file->uid)->pw_name);
+         char *ow = owner_name(file->uid);
+         ft_printf("%s", ow);
+         print_pad(wo - ft_strlen(ow));
          ft_printf("  ");
-         ft_printf("%s", getgrgid(file->gid)->gr_name);
+         char *gr = group_name(file->gid);
+         ft_printf("%s", gr);
+         print_pad(wg - ft_strlen(gr));
          ft_printf("  ");
+         print_pad(ws - num_width(file->size));
          ft_printf("%ld", file->size);
          ft_printf(" ");
          char *time_str = ctime(&file->mtime);
@@ -58,7 +119,7 @@ char *build_path(char *dir, char *name)
    return full;
 }
 
-void list_dir(char *path)
+void list_dir(char *path, int *flags)
 {
    DIR *dir;
    struct dirent *entry;
@@ -99,10 +160,10 @@ void list_dir(char *path)
       free(full);
    }
    closedir(dir);
-   printfiles(files, 0);
+   printfiles(files, flags[FLAG_L]);
 }
 
-void lister(t_list *targets)
+void lister(t_list *targets, int *flags)
 {
    t_list *tmp = targets;
    t_list *files = NULL;
@@ -138,12 +199,12 @@ void lister(t_list *targets)
       tmp = tmp->next;
    }
 
-   printfiles(files, 0);
+   printfiles(files, flags[FLAG_L]);
    t_list *tmp2 = dirs;
    while (tmp2)
    {
       t_file *file = (t_file *)tmp2->content;
-      list_dir(file->file_name);
+      list_dir(file->file_name, flags);
 
       tmp2 = tmp2->next;
    }
@@ -160,7 +221,7 @@ int main(int argc, char *argv[])
    targets = parser(argv, flags_selected);
    if (!targets) return 1;
    // print_parser_result(targets, flags_selected);
-   lister(targets);
+   lister(targets, flags_selected);
    ft_lstclear(&targets, free);
    return 0;
 }
